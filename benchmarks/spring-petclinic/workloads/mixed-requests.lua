ff0871d3351d15665b590720b77ade2a8f743b4e
Andrija Kolic <andrija.kolic@oracle.com>
2025-04-09 14:33:23 +0200

    Fix GC bound workloads (Micronaut Shopcart and Spring Petclinic); switch from single long warmup iteration to multiple short warmup iterations to enable monitoring of warmup curves.

--- benchmarks/spring-petclinic/workloads/mixed-requests.lua
+++ benchmarks/spring-petclinic/workloads/mixed-requests.lua
@@ -21,63 +21,83 @@ end
 
 local ownerCounter
 local petCounter
+local loopCounter
 function init(args)
    ownerCounter = 1000000000000000 + 10000000000000 * threadId
    petCounter = ownerCounter
+   loopCounter = ownerCounter
 end
 
 local ownerPath
 local petId
-local state = -1
+local state = -5
 function request()
-    if state == 0 then
+    -- Setup steps
+    if state == -5 then
+        -- workaround as the response handler is not invoked for the very first request
+        method = "GET"
+        path = "/"
+        headers = getHeaders
+        body = ""
+    elseif state == -4 then
         -- create a new owner at http://localhost:8006/owners/new
         ownerCounter = ownerCounter + 1
         method = "POST"
         path = "/owners/new"
         headers = postHeaders
         body = string.format(newOwnerRequest, ownerCounter, ownerCounter, ownerCounter, ownerCounter, ownerCounter / 10000000)
-    elseif state == 1 then
+    elseif state == -3 then
         -- create a new pet for the current owner at http://localhost:8006/owners/%d/pets/new
         petCounter = petCounter + 1
         method = "POST"
         path = ownerPath .. "/pets/new"
         headers = postHeaders
         body = string.format(newPetRequest, petCounter, petTypes[(petCounter % #petTypes) + 1])
-    elseif state == 2 then
-        -- navigate to http://localhost:8006/owners/%d
-        method = "GET"
-        path = ownerPath
-        headers = getHeaders
-        body = ""
-    elseif state == 3 then
-        -- create a new visit for the current pet at http://localhost:8006/owners/%d/pets/%d/visits/new
-        method = "POST"
-        path = ownerPath .. "/pets/" .. petId .. "/visits/new"
-        headers = postHeaders
-        body = string.format(newVisitRequest)
-    elseif state == 4 then
+    elseif state == -2 then
         -- navigate to http://localhost:8006/owners/%d
         method = "GET"
         path = ownerPath
         headers = getHeaders
         body = ""
     elseif state == -1 then
-        -- workaround as the response handler is not invoked for the very first request
+        -- create a new visit for the current pet at http://localhost:8006/owners/%d/pets/%d/visits/new
+        method = "POST"
+        path = ownerPath .. "/pets/" .. petId .. "/visits/new"
+        headers = postHeaders
+        body = string.format(newVisitRequest)
+    -- Main loop
+    elseif state == 0 then
+        -- navigate to http://localhost:8006/owners/%d
         method = "GET"
-        path = "/"
+        path = ownerPath
         headers = getHeaders
         body = ""
+    elseif state == 1 then
+        -- navigate to http://localhost:8006/owners/%d/edit
+        method = "POST"
+        path = ownerPath .. "/edit"
+        headers = postHeaders
+        body = string.format(newOwnerRequest, loopCounter, loopCounter, loopCounter, loopCounter, loopCounter / 10000000)
+    elseif state == 2 then
+        -- navigate to http://localhost:8006/owners/%d/pets/%d/edit
+        method = "POST"
+        path = ownerPath .. "/pets/" .. petId .. "/edit"
+        headers = postHeaders
+        body = string.format(newPetRequest, loopCounter, petTypes[(loopCounter % #petTypes) + 1])
+        loopCounter = loopCounter + 1
     end
     
-    state = (state + 1) % 5
+    state = state + 1
+    if state > 0 then
+        state = state % 3
+    end
     return wrk.format(method, path, headers, body)
 end
 
 function response(status, headers, body)
-    if state == 1 then
+    if state == -3 then
         ownerPath = string.sub(headers["Location"], hostUrlLength + 1, -1)
-    elseif state == 3 then
+    elseif state == -1 then
         petId = string.match(body, 'pets/(%d+)/visits/new')
     end
 end
